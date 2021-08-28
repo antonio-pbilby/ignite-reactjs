c571d543cf6bc35d3ec7e0fe30718a62c067cd60
Antônio Bilby <antoniobilbylemos@gmail.com>
2021-08-28 20:27:13 -0300

    CII 4.7

--- dtmoney/src/components/NewTransactionModal/styles.ts
+++ dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from 'polished';
+import { darken } from "polished";
 
 export const Container = styled.form`
   h2 {
@@ -33,7 +33,7 @@ export const Container = styled.form`
     padding: 0 1.5rem;
     height: 4rem;
     background: var(--green);
-    color: #FFF;
+    color: #fff;
     border-radius: 0.25rem;
     border: 0;
     font-size: 1rem;
@@ -47,41 +47,45 @@ export const Container = styled.form`
       filter: brightness(0.9);
     }
   }
-` 
+`;
 
 export const TransactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   margin: 1rem 0;
   gap: 0.5rem;
+`;
 
-  button {
-    height: 4rem;
-    border: 1px solid #d7d7d7;
+interface RadioBoxProps {
+  isActive: boolean;
+}
 
-    border-radius: 0.25rem;
-    background: transparent;
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  background: ${(props) => props.isActive ? '#e7e9ee' : 'transparent'};
 
-    transition: border-color 0.2s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    &:hover {
-      border-color: ${darken(0.1, '#d7d7d7')};
-    }
+  transition: border-color 0.2s;
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  &:hover {
+    border-color: ${darken(0.1, "#d7d7d7")};
+  }
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `;
