d6caff6b2c41105577796cd21f7dafcdad193e27
Antonio Lemos <antonio.lemos@ibm.com>
2021-12-20 16:18:19 -0300

    chapter 3.2.2

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,10 +12,33 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
       authorization: {
         params: {
-          scope: 'read:user'
-        }
-      }
+          scope: "read:user",
+        },
+      },
     }),
     // ...add more providers here
   ],
-})
+  // jwt: {
+  //   secret: process.env.JWT_SIGNIN_KEY
+  // },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const userEmail = user.email;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(userEmail)))
+            ),
+            q.Create(q.Collection("users"), { data: { email: userEmail } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(userEmail)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
